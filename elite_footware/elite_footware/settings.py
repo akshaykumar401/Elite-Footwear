d521c562aeb184bb88bddd4c3db617191988e39b
akshaykumar401 <akshaykumar835103@gmail.com>
2026-06-13 11:07:00 +0530

    Adding New Apps in Project

--- elite_footware/elite_footware/settings.py
+++ elite_footware/elite_footware/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "product",
+    "about_us",
+    "contact_us",
 ]
 
 MIDDLEWARE = [
--- elite_footware/elite_footware/urls.py
+++ elite_footware/elite_footware/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("product/", include("product.urls")),
+    path("about_us/", include("about_us.urls")),
+    path("contact_us/", include("contact_us.urls")),
 ]
